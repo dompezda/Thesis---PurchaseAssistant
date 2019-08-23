c34295accfcb5f2ef245d3f46521c2944de99a44
Dominik <dominik.pezda96@gmail.com>
2019-08-23 16:11:54 +0200

    edit and frequent
    add and delete product in edit repair, frequent products partial

---
+++ login/Assistant/Assistant/Controllers/GenerateController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Assistant.Models;
+using System.Net;
+using Assistant.Data;
+using Assistant.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assistant.Controllers
+{
+    public class GenerateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Select_list()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Generate()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                int amount=db.ProductLists.Count();
+
+
+            }
+
+
+
+                return View();
+        }
+    }
+}
